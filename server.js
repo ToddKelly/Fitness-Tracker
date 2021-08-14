d65a8c49b4935bb65c09bb5eae5605c3fd777025
Todd Kelly <donkeyskates21@gmail.com>
2021-08-14 18:10:43 -0400

    README

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const logger = require("morgan");
 //const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
